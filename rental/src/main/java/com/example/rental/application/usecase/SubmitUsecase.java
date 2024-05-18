547577b1e29de0dbb71bb0a80f6159e6d4ac8694
seriouskang <scdean110@gmail.com>
2024-05-18 23:23:02 +0900

    feat(application): submit usecase 신규

---
+++ rental/src/main/java/com/example/rental/application/usecase/SubmitUsecase.java
@@ -0,0 +1,8 @@
+package com.example.rental.application.usecase;
+
+import com.example.rental.framework.httpadapter.dto.RentalCardOutputDTO;
+import com.example.rental.framework.httpadapter.dto.UserItemInputDTO;
+
+public interface SubmitUsecase {
+    public RentalCardOutputDTO submit(UserItemInputDTO submit);
+}
