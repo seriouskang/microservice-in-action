ad6a12b959628ea1471aabab881de3e25e338072
seriouskang <scdean110@gmail.com>
2024-05-18 23:25:23 +0900

    feat(application): overdue usecase 신규

--- rental/src/main/java/com/example/rental/application/usecase/OverdueUsecase.java
+++ rental/src/main/java/com/example/rental/application/usecase/OverdueUsecase.java
@@ -3,6 +3,6 @@ package com.example.rental.application.usecase;
 import com.example.rental.framework.httpadapter.dto.RentalCardOutputDTO;
 import com.example.rental.framework.httpadapter.dto.UserItemInputDTO;
 
-public interface RentUsecase {
-    public RentalCardOutputDTO rent(UserItemInputDTO userItemInputDTO);
+public interface OverdueUsecase {
+    public RentalCardOutputDTO overdue(UserItemInputDTO userItemInputDTO);
 }
