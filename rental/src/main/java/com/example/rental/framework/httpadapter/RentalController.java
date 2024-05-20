b5bde4d1d209e889a82b3d36d67ecf8d2c176a18
seriouskang <scdean110@gmail.com>
2024-05-20 22:38:51 +0900

    feat(rental): RentalController 신규

---
+++ rental/src/main/java/com/example/rental/framework/httpadapter/RentalController.java
@@ -0,0 +1,58 @@
+package com.example.rental.framework.httpadapter;
+
+import com.example.rental.application.port.in.*;
+import com.example.rental.framework.httpadapter.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/rental")
+@RequiredArgsConstructor
+public class RentalController {
+    private final RentUsecase rentUsecase;
+    private final SubmitUsecase submitUsecase;
+    private final CreateRentalCardUsecase createRentalCardUsecase;
+    private final OverdueUsecase overdueUsecase;
+    private final ClearOverdueUsecase clearOverdueUsecase;
+    private final InquiryUsecase inquiryUsecase;
+
+    @PostMapping("/rental-card")
+    public ResponseEntity<RentalCardOutputDTO> createRentalCard(@RequestBody UserInputDTO userInputDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createRentalCardUsecase.createRentalCard(userInputDTO));
+    }
+
+    @GetMapping("/rental-card/{userId}")
+    public ResponseEntity<RentalCardOutputDTO> rentalCard(@PathVariable Long userId) {
+        Optional<RentalCardOutputDTO> rentalCard = inquiryUsecase.getRentalCard(new UserInputDTO(userId, ""));
+        return ResponseEntity
+                .ok(rentalCard.get());  // @TODO: validate
+    }
+
+    @GetMapping("/{userId}/rental-items")
+    public ResponseEntity<List<RentalItemOutputDTO>> allRentalItems(@PathVariable Long userId) {
+        Optional<List<RentalItemOutputDTO>> allRentalItems = inquiryUsecase.getAllRentalItems(new UserInputDTO(userId, ""));
+        return ResponseEntity
+                .ok(allRentalItems.get());  // @TODO: validate
+    }
+
+    @GetMapping("/{userId}/submit-items")
+    public ResponseEntity<List<SubmitItemOutputDTO>> allSubmitItems(@PathVariable Long userId) {
+        Optional<List<SubmitItemOutputDTO>> allSubmitItems = inquiryUsecase.getAllSubmitItems(new UserInputDTO(userId, ""));
+        return ResponseEntity
+                .ok(allSubmitItems.get());  // @TODO: validate
+    }
+
+    @PostMapping("/rent")
+    public ResponseEntity<RentalCardOutputDTO> rent(@RequestBody UserItemInputDTO userItemInputDTO) {
+        RentalCardOutputDTO rentalCardOutputDTO = rentUsecase.rent(userItemInputDTO);
+        return ResponseEntity
+                .ok(rentalCardOutputDTO);
+    }
+}
