e40e4738acb0ab695555d3d87c0b3eb8bc04972b
seriouskang <scdean110@gmail.com>
2024-05-26 11:49:29 +0900

    feat(book): BookOutputDTO getter 추가

--- book/src/main/java/com/example/book/framework/httpadapter/dto/BookOutputDTO.java
+++ book/src/main/java/com/example/book/framework/httpadapter/dto/BookOutputDTO.java
@@ -3,7 +3,9 @@ package com.example.book.framework.httpadapter.dto;
 import com.example.book.domain.model.Book;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookOutputDTO {
     private long id;
