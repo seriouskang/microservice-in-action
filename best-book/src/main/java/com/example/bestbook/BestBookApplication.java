164f454fb08bbba677979fb299fc02f73724227c
seriouskang <scdean110@gmail.com>
2024-05-26 22:28:39 +0900

    feat(bestbook): best-book 프로젝트 신규

---
+++ best-book/src/main/java/com/example/bestbook/BestBookApplication.java
@@ -0,0 +1,11 @@
+package com.example.bestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BestBookApplication.class, args);
+    }
+}
