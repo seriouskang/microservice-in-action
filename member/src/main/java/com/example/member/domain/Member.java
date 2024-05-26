c3501af84c8d1437c571925616274a7f10abee85
seriouskang <scdean110@gmail.com>
2024-05-26 12:55:03 +0900

    feat(member): 도메인 객체 비즈니스 로직 추가

--- member/src/main/java/com/example/member/domain/Member.java
+++ member/src/main/java/com/example/member/domain/Member.java
@@ -2,13 +2,45 @@ package com.example.member.domain;
 
 import com.example.member.domain.vo.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Member {
     private Long id;
     private IdName idName;
-    private Password password;
+    private Password pwd;
     private Email email;
     private List<Authority> authorities;
     private Point point;
+
+    private Member(IdName idName, Password pwd, Email email) {
+        this.idName = idName;
+        this.pwd = pwd;
+        this.email = email;
+
+        this.point = new Point();
+        this.authorities = new ArrayList<>();
+        addAuthority(Authority.of(UserRole.USER));
+    }
+
+    public void addAuthority(Authority auth) {
+        this.authorities.add(auth);
+    }
+
+    public void addPoint(Long point) {
+        this.point.add(point);
+    }
+
+    public void usePoint(Long point) {
+        this.point.deduct(point);
+    }
+
+    // @TODO
+    public Member login(IdName idName, Password pwd) {
+        return this;
+    }
+
+    // @TODO
+    public void logout(IdName idName) {
+    }
 }
--- member/src/main/java/com/example/member/domain/vo/Authority.java
+++ member/src/main/java/com/example/member/domain/vo/Authority.java
@@ -1,5 +1,13 @@
 package com.example.member.domain.vo;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Authority {
     private UserRole userRole;
+
+    public static Authority of(UserRole userRole) {
+        return new Authority(userRole);
+    }
 }
