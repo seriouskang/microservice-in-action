6b03ba8c7414e6295dfb9dc85d8236ca94f57bba
seriouskang <scdean110@gmail.com>
2024-05-27 23:50:34 +0900

    feat(bestbook): application.yaml mongodb 관련 설정 추가

--- best-book/src/main/java/com/example/bestbook/domain/model/vo/Item.java
+++ best-book/src/main/java/com/example/bestbook/domain/model/vo/Item.java
@@ -1,8 +1,10 @@
 package com.example.bestbook.domain.model.vo;
 
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
+@Getter
 public class Item {
     private long no;
     private String title;
